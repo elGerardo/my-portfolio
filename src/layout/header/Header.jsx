fa4e65f8fef8fbf4e7fbc4a59ecb83474d260e67
elGerardo <gerardoyu54@gmail.com>
2022-12-02 23:48:00 -0800

    netlify toml added, safari css added, header margin changed

--- src/layout/header/Header.jsx
+++ src/layout/header/Header.jsx
@@ -4,13 +4,15 @@ import { Link } from "react-router-dom";
 
 let Header = () => {
     let content = (
-        <Container className={`${style.container} py-3 w-75 position-fixed d-flex justify-content-between`}>
+        <div className={`${style.container} py-3 w-100 position-fixed`}>
+            <Container className={`${style.content} d-flex justify-content-between`}>
             <Link to="/" style={{color:"#FFFFFF", textDecoration:"none"}}><h1>Gerardo</h1></Link>
             <div>
                 <Link to="about" style={{color:"#FFFFFF", textDecoration:"none"}}>about me</Link>
-                <Link to="projects" style={{color:"#FFFFFF", textDecoration:"none"}} className={`mx-5`}>my projects</Link>
+                <Link to="projects" style={{color:"#FFFFFF", textDecoration:"none"}} className={`mx-3`}>my projects</Link>
             </div>
-        </Container>
+            </Container>
+        </div>
     );
 
     return content;
